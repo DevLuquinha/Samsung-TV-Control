98a15061eefc58f762118974085d009bcc9a1858
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-08-02 14:12:11 -0300

    BUILD: Add the libs to the app.py

---
+++ python-server/app.py
@@ -0,0 +1,9 @@
+# Importa o Flask para criar o servidor
+from flask import Flask, request, jsonify
+
+# Importa a biblioteca da samsung para controlar a TV
+import samsungctl
+
+# Importa a biblioteca que para carregar o arquivo de configuração  
+import json
+
